c049c5de5552eab66f0c921892ae425f17231467
Yonit Chauhan <72747510+yonit-chauhan@users.noreply.github.com>
2022-05-30 12:09:26 +0530

    Added test comment for jenkins webhook

--- src/main/java/com/yellow/login/User.java
+++ src/main/java/com/yellow/login/User.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 @Entity //Tells that this is a SQL table
 public class User {
 
+    //Test comment
     public static final char SEX_MALE = 'm';
     public static final char SEX_FEMALE = 'f';
     public static final String USER_TYPE_ADMIN = "admin";
